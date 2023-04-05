2e2a54400ae41adb7d31c332857f477b8cac9447
bugRanger <departmentofjusticeevil@gmail.com>
2023-04-05 18:01:41 +0700

    Fix default background color

--- src/MagicMaze.Core/Entities/MazeColorSettings.cs
+++ src/MagicMaze.Core/Entities/MazeColorSettings.cs
@@ -5,7 +5,7 @@ namespace MagicMaze.Core.Entities
     public class MazeColorSettings
     {
         private static Color DARK_GRAY => Color.FromArgb(1, 20, 20, 20);
-        private static Color BLACK_GRAY => Color.FromArgb(1, 33, 33, 33);
+        private static Color BLACK_GRAY => Color.FromArgb(1, 70, 70, 70);
         private static Color LIGHT_BLUE => Color.FromArgb(1, 61, 109, 153);
 
         public static MazeColorSettings Default => new MazeColorSettings(BLACK_GRAY, LIGHT_BLUE, DARK_GRAY);
